793e60d472ea52731f033c9c3fdc3fa50dfcfe7f
Aaron Van De Brook <avandebrook5@gmail.com>
2020-08-11 19:48:41 -0500

    Added functionality to write to an output file and refactored Manager.

--- source/DotFileWriter/DotFileWriter.java
+++ source/DotFileWriter/DotFileWriter.java
@@ -1,5 +1,6 @@
 package source.DotFileWriter;
 
+import java.io.*;
 import java.util.LinkedList;
 
 import source.DotGraphParser.*;
@@ -7,6 +8,7 @@ import source.DotGraphParser.*;
 public class DotFileWriter
 {
     protected TemplateReader templateReader;
+    private BufferedWriter writer;
     private String graphType;
     private String graphName;
     private String graphTemplate;
@@ -21,25 +23,47 @@ public class DotFileWriter
         this.graphType = graphType;
         this.graphName = graphName;
 
-        graphTemplate = templateReader.readGraphTemplate(true);
-        subgraphTemplate = templateReader.readSubgraphTemplate(true);
+        graphTemplate = templateReader.readGraphTemplate(false);
+        subgraphTemplate = templateReader.readSubgraphTemplate(false);
     }
 
     public String buildGraph(LinkedList<GraphContainer> graphList)
     {
         String graph = graphTemplate;
         LinkedList<String> subgraphList = buildSubgraphs(graphList);
+        LinkedList<String> connectionsList = buildConnections(graphList);
         String subgraphs = "";
+        String connections = "";
 
         for (String s : subgraphList) {
             subgraphs += s;
         }
 
+        for (String s : connectionsList) {
+            connections += s + ";";
+        }
+
         graph = graph.replaceAll("%graphType%", graphType);
         graph = graph.replaceAll("%graphName%", graphName);
         graph = graph.replaceAll("%subgraph%", subgraphs);
+        graph = graph.replaceAll("%nodeConnections%", connections);
 
         return graph;
+    }
+
+    public void writeGraphToFile(String dir, LinkedList<GraphContainer> graphList)
+    {
+        String graph = buildGraph(graphList);
+        try {
+            writer = new BufferedWriter(new FileWriter(dir));
+
+            for (char c : graph.toCharArray())
+                writer.write(c);
+
+            writer.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     private LinkedList<String> buildSubgraphs(LinkedList<GraphContainer> graphList)
@@ -57,10 +81,26 @@ public class DotFileWriter
             subgraph = subgraph.replaceAll("%subgraphLabel%", graph.getGraphName());
             subgraph = subgraph.replaceAll("%subgraphName%", "cluster_" + graph.getGraphName());
             subgraph = subgraph.replaceAll("%nodeDefinitions%", nodeDefinitions);
-            subgraphList.add("\t" + subgraph);
+            subgraphList.add(subgraph);
         }
 
         return subgraphList;
+    }
+
+    private LinkedList<String> buildConnections(LinkedList<GraphContainer> graphList)
+    {
+        LinkedList<String> connectionsList = new LinkedList<String>();
+
+        // TODO: Worst case O(n^3), might need to make a faster algorithm later
+        for (GraphContainer graph : graphList) {
+            for (GraphNodeContainer node : graph.getNodeList()) {
+                for (GraphNodeContainer connection : node.getConnections()) {
+                    connectionsList.add(node.getNodeName() + " -> " + connection.getNodeName());
+                }
+            }
+        }
+
+        return connectionsList;
     }
 
     /********** Accessor Functions **********/
--- source/Manager.java
+++ source/Manager.java
@@ -1,6 +1,7 @@
 package source;
 
 import java.util.LinkedList;
+import java.io.*;
 
 import source.DotFileEnumerator.*;
 import source.DotGraphParser.*;
@@ -15,39 +16,45 @@ public class Manager
             LinkedList<GraphContainer> list = parser.parse();
             DotFileWriter graphWriter = new DotFileWriter("digraph", "dot_expander_test");
 
-            System.out.println(graphWriter.buildGraph(list));
+            graphWriter.writeGraphToFile("out/graph.dot", list);
+
+            // System.out.println(graphWriter.buildGraph(list));
             // System.out.println(graphWriter.getGraphTemplate());
             // System.out.println(graphWriter.getSubgraphTemplate());
-
-            // for (GraphContainer container : list) {
-            //     System.out.println("\nFile Name: " + container.getFileName());
-            //     System.out.println("Graph Name: " + container.getGraphName());
-            //     System.out.println("Graph Prefix: " + container.getGraphPrefix());
-
-            //     for (GraphNodeContainer node : container.getNodeList()) {
-            //         System.out.println("Node ID: " + node.getNodeID());
-            //         System.out.println("Node name: " + node.getNodeName());
-            //         System.out.println("Node prefix: " + node.getNodePrefix());
-            //     }
-
-            //     System.out.println("Connections:");
-            //     for (String s : container.getEdgeList()) {
-            //         System.out.println(s);
-            //     }
-
-            //     System.out.println("Connections:");
-            //     for (GraphNodeContainer node : container.getNodeList()) {
-            //         System.out.println("\n" + node.getNodeID() + ":");
-            //         for (GraphNodeContainer s : node.getConnections()) {
-            //             System.out.println("-" + s.getNodeID());
-            //         }
-            //     }
-            // }
+            // printDebugInfo(list);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.err.println("Usage:java source.Manager [directory]");
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void printDebugInfo(LinkedList<GraphContainer> list)
+    {
+        for (GraphContainer container : list) {
+            System.out.println("\nFile Name: " + container.getFileName());
+            System.out.println("Graph Name: " + container.getGraphName());
+            System.out.println("Graph Prefix: " + container.getGraphPrefix());
+
+            for (GraphNodeContainer node : container.getNodeList()) {
+                System.out.println("Node ID: " + node.getNodeID());
+                System.out.println("Node name: " + node.getNodeName());
+                System.out.println("Node prefix: " + node.getNodePrefix());
+            }
+
+            System.out.println("Connections:");
+            for (String s : container.getEdgeList()) {
+                System.out.println(s);
+            }
+
+            System.out.println("Connections:");
+            for (GraphNodeContainer node : container.getNodeList()) {
+                System.out.println("\n" + node.getNodeID() + ":");
+                for (GraphNodeContainer s : node.getConnections()) {
+                    System.out.println("-" + s.getNodeID());
+                }
+            }
         }
     }
 }
